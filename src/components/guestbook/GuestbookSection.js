0558e68f6276f3d33836192fb2339a250b7d8697
aptheparker <aptheparker@gmail.com>
2023-07-26 17:06:27 +0900

    container changed to grid layout && comments can be added

--- src/components/guestbook/GuestbookSection.js
+++ src/components/guestbook/GuestbookSection.js
@@ -1,35 +1,39 @@
-import GuestbookAddButton from "../../assets/images/guestbook_add_button.png";
+import React, { useState } from "react";
 import classes from "./GuestbookSection.module.css";
 import NewCommentForm from "./NewCommentForm";
 import CommentPreviewContainer from "./CommentPreviewContainer";
-import { useState } from "react";
-
-//여기서 무한스크롤기능 구현하기
 
 const GuestbookSection = () => {
   const [openForm, setOpenForm] = useState(false);
+  const [comments, setComments] = useState([]); // track all comments
+
   const openFormHandler = () => {
     setOpenForm(true);
   };
+
   const closeFormHandler = () => {
     setOpenForm(false);
   };
+
+  const addNewCommentHandler = (comment) => {
+    setComments((prevComments) => [...prevComments, comment]);
+    // Close the form after submitting
+    closeFormHandler();
+  };
+
   return (
     <>
-      {openForm && <NewCommentForm cancelHandler={closeFormHandler} />}
+      {openForm && <NewCommentForm cancelHandler={closeFormHandler} onAddComment={addNewCommentHandler} />}
       <div className={classes["guestbook-contents"]}>
         <div className={classes["guestbook-titles"]}>
           <div className={classes["guestbook-title"]}>방명록을 남겨주세요</div>
           <div className={classes["guestbook-subtitle"]}>
             비방 / 욕설은 관리자에 의해 삭제될 수 있습니다.
           </div>
-          <button
-            className={classes["guestbook-add-button"]}
-            onClick={openFormHandler}
-          />
+          <button className={classes["guestbook-add-button"]} onClick={openFormHandler} />
         </div>
 
-        <CommentPreviewContainer />
+        <CommentPreviewContainer comments={comments} />
       </div>
     </>
   );
--- src/components/guestbook/NewCommentForm.js
+++ src/components/guestbook/NewCommentForm.js
@@ -1,11 +1,20 @@
+// imports
+import { useState } from "react";
+
+// components
 import CommentModal from "./CommentModal";
 import classes from "./NewCommentForm.module.css";
-// import { Form } from "react-router-dom";
 import submitButton from "../../assets/images/guestbook_submit_button.svg";
 import cancelButton from "../../assets/images/guestbook_cancel_button.svg";
-const NewCommentForm = ({ cancelHandler }) => {
-  const onSubmitHandler = () => {
-    //받은 content 여기에서 DB에 바로 전달
+
+const NewCommentForm = ({ cancelHandler, onAddComment }) => {
+  const [comment, setComment] = useState("");
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (comment.trim() === "") return; // Prevent empty comments
+    onAddComment({ id: Date.now(), content: comment });
+    setComment(""); // Clear the form
   };
 
   return (
@@ -15,8 +24,16 @@ const NewCommentForm = ({ cancelHandler }) => {
           <img src={cancelButton} alt="cancel-button" />
         </div>
         <form className={classes["form"]}>
-          <label htmlFor="comment" style={{marginLeft: 20, marginTop: 40}}>Comment</label>
-          <textarea className={classes["textarea"]} required rows={3} />
+          <label htmlFor="comment" style={{ marginLeft: 20, marginTop: 40 }}>
+            Comment
+          </label>
+          <textarea
+            className={classes["textarea"]}
+            required
+            rows={3}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
         </form>
 
         <div className={classes["submit-button"]} onClick={onSubmitHandler}>
@@ -32,3 +49,31 @@ const NewCommentForm = ({ cancelHandler }) => {
 };
 
 export default NewCommentForm;
+
+//   return (
+//     <CommentModal>
+//       <div className={classes["container"]}>
+//         <div className={classes["cancel-button"]} onClick={cancelHandler}>
+//           <img src={cancelButton} alt="cancel-button" />
+//         </div>
+//         <form className={classes["form"]} onSubmit={onSubmitHandler}>
+//           <label htmlFor="comment" style={{ marginLeft: 20, marginTop: 40 }}>
+//             Comment
+//           </label>
+//           <textarea
+//             className={classes["textarea"]}
+//             required
+//             rows={3}
+//             value={comment}
+//             onChange={(e) => setComment(e.target.value)}
+//           />
+//           <div className={classes["submit-button"]} onClick={onSubmitHandler}>
+//             <img className={classes["submit-button_img"]} src={submitButton} alt="submit-button" />
+//           </div>
+//         </form>
+//       </div>
+//     </CommentModal>
+//   );
+// };
+
+// export default NewCommentForm;
