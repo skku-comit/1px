2d70b2e8bdf33afcf7b513edf929c500ecaeb8fc
aptheparker <aptheparker@gmail.com>
2023-07-26 16:43:00 +0900

    button image changed

--- src/components/guestbook/GuestbookSection.js
+++ src/components/guestbook/GuestbookSection.js
@@ -6,7 +6,6 @@ import { useState } from "react";
 
 //여기서 무한스크롤기능 구현하기
 
-
 const GuestbookSection = () => {
   const [openForm, setOpenForm] = useState(false);
   const openFormHandler = () => {
@@ -24,12 +23,10 @@ const GuestbookSection = () => {
           <div className={classes["guestbook-subtitle"]}>
             비방 / 욕설은 관리자에 의해 삭제될 수 있습니다.
           </div>
-        </div>
-        <div
-          className={classes["guestbook-add-button"]}
-          onClick={openFormHandler}
-        >
-          <img src={GuestbookAddButton} alt="guestbook-add-button" />
+          <button
+            className={classes["guestbook-add-button"]}
+            onClick={openFormHandler}
+          />
         </div>
 
         <CommentPreviewContainer />
