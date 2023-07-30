bf608c9e8a59d28c27db0e4a12e8ab9eeb119175
aptheparker <aptheparker@gmail.com>
2023-07-30 21:25:16 +0800

    firebase fetch order added

--- src/components/guestbook/CommentPreviewContainer.js
+++ src/components/guestbook/CommentPreviewContainer.js
@@ -1,36 +1,48 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 
 // firebase
-import {getDocs} from 'firebase/firestore'
+import { getDocs, query, orderBy } from "firebase/firestore";
 import classes from "./CommentPreviewContainer.module.css";
 import CommentPreviewCard from "./CommentPreviewCard";
 
 function CommentPreviewContainer({ collectionRef }) {
-  const [comments, setComments] = useState([])
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     const getComment = async () => {
-      await getDocs(collectionRef).then((comment) => {
-        let commentData = comment.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-        setComments(commentData)
-        }).catch((err) => {
-          console.log(err);
-        })
+      try {
+        // Create a query to fetch the comments collection ordered by the 'timestamp' field
+        const q = query(collectionRef, orderBy("timestamp", "desc"));
+
+        // Fetch the documents based on the query
+        const querySnapshot = await getDocs(q);
+
+        // Map the data and set the comments state
+        const commentData = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setComments(commentData);
+      } catch (err) {
+        console.log(err);
       }
-    getComment()
-    }, [])
+    };
+    getComment();
+  }, []);
 
   return (
     <>
       {comments.length === 0 && (
         <div className={classes["container2"]}>
           <div style={{ textAlign: "center", color: "black" }}>
-            <h4 className={classes['no-text']}>방명록에 첫번째 글을 남겨보세요!</h4>
+            <h4 className={classes["no-text"]}>
+              방명록에 첫번째 글을 남겨보세요!
+            </h4>
           </div>
         </div>
       )}
       <div className={classes["container"]}>
-        {comments.map(({comment,id}) => (
+        {comments.map(({ comment, id }) => (
           <CommentPreviewCard key={id} content={comment} />
         ))}
       </div>
