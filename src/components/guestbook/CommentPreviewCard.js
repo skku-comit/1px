daf72fbb2225259e027e158e631bbb6f37afb49b
aptheparker <aptheparker@gmail.com>
2023-07-30 22:53:11 +0800

    admin code added for delete function

--- src/components/guestbook/CommentPreviewCard.js
+++ src/components/guestbook/CommentPreviewCard.js
@@ -4,24 +4,28 @@ import { deleteDoc, doc } from "firebase/firestore";
 import classes from "./CommentPreviewCard.module.css";
 
 function CommentPreviewCard({ id, content, db }) {
-
   const deleteComment = async () => {
     try {
-      const confirmDelete = window.confirm("Are you sure you want to delete this Comment?");
-      if (confirmDelete) {
+      const confirmDelete = window.prompt("관리자 코드: ");
+      if (confirmDelete == "0707") {
         const documentRef = doc(db, "comments", id);
         await deleteDoc(documentRef);
         window.location.reload();
+      } else {
+        alert("틀렸습니다.");
       }
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className={classes["comment-card"]}>
       <div className={classes["comment-text"]}>{content}</div>
-      <button className={classes["delete-button"]} onClick={() => deleteComment()}/>
+      <button
+        className={classes["delete-button"]}
+        onClick={() => deleteComment()}
+      />
     </div>
   );
 }
