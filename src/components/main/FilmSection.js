e187d905e5e44ecdd52d85c5e60bfedf8b7a2d57
aptheparker <aptheparker@gmail.com>
2023-08-05 14:37:24 +0800

    film style refactored

--- src/components/main/FilmSection.js
+++ src/components/main/FilmSection.js
@@ -1,8 +1,15 @@
+// imports
 import React from "react";
-import classes from "./Filmsection.module.css";
-import Film from "./Film";
-import { ReactComponent as Filmbody } from "../../assets/images/film_body.svg";
+import { Link } from "react-router-dom";
 
+// components
+import Film from "./Film";
+
+// css
+import classes from "./Filmsection.module.css";
+
+// images
+import { ReactComponent as Filmbody } from "../../assets/images/film_body.svg";
 import film1 from "../../assets/images/film1.png";
 import film2 from "../../assets/images/film2.png";
 import film3 from "../../assets/images/film3.png";
@@ -10,12 +17,6 @@ import film4 from "../../assets/images/film4.png";
 import film5 from "../../assets/images/film5.png";
 import film6 from "../../assets/images/film6.png";
 import film7 from "../../assets/images/film7.png";
-import { Link } from "react-router-dom";
-
-const filmStyle = {
-  width: "100%",
-  height: "100%",
-};
 
 const FilmSection = React.forwardRef((props, ref) => {
   return (
@@ -25,7 +26,11 @@ const FilmSection = React.forwardRef((props, ref) => {
         <Film id="film-1" onClick={props.onClickFilm}>
           <Link to={"./iframe1"}>
             Section1
-            <img src={film1} alt="The Devil Wears Prada" style={filmStyle} />
+            <img
+              src={film1}
+              alt="The Devil Wears Prada"
+              className={classes["film-img"]}
+            />
           </Link>
         </Film>
         <Film id="film-2" onClick={props.onClickFilm}>
@@ -34,7 +39,7 @@ const FilmSection = React.forwardRef((props, ref) => {
             <img
               src={film2}
               alt="Threads: Our Tapestry of Love"
-              style={filmStyle}
+              className={classes["film-img"]}
             />
           </Link>
         </Film>
@@ -44,14 +49,18 @@ const FilmSection = React.forwardRef((props, ref) => {
             <img
               src={film3}
               alt="Everything Everywhere All at Once"
-              style={filmStyle}
+              className={classes["film-img"]}
             />
           </Link>
         </Film>
         <Film id="film-4" onClick={props.onClickFilm}>
           <Link to={"./iframe4"}>
             Section4
-            <img src={film4} alt="Romantic Holiday" style={filmStyle} />
+            <img
+              src={film4}
+              alt="Romantic Holiday"
+              className={classes["film-img"]}
+            />
           </Link>
         </Film>
         <Film id="film-5" onClick={props.onClickFilm}>
@@ -60,20 +69,28 @@ const FilmSection = React.forwardRef((props, ref) => {
             <img
               src={film5}
               alt="Everything Everywhere All at Once"
-              style={filmStyle}
+              className={classes["film-img"]}
             />
           </Link>
         </Film>
         <Film id="film-6" onClick={props.onClickFilm}>
           <Link to={"./iframe6"}>
             Section6
-            <img src={film6} alt="Legally Blonde" style={filmStyle} />
+            <img
+              src={film6}
+              alt="Legally Blonde"
+              className={classes["film-img"]}
+            />
           </Link>
         </Film>
         <Film id="film-7" onClick={props.onClickFilm}>
           <Link to={"./iframe7"}>
             Section7
-            <img src={film7} alt="The French Dispatch" style={filmStyle} />
+            <img
+              src={film7}
+              alt="The French Dispatch"
+              className={classes["film-img"]}
+            />
           </Link>
         </Film>
       </div>
