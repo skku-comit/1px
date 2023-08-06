558ae3bb195ee880a4e7b8969931d5f71f9ab445
aptheparker <aptheparker@gmail.com>
2023-08-06 11:27:41 +0800

    iframe page code refactored

--- src/components/main/ScrollButton.js
+++ src/components/main/ScrollButton.js
@@ -3,11 +3,11 @@ import classes from "./ScrollButton.module.css";
 const LeftScrollButton = ({ onLeftScrollButtonClick }) => {
   return (
     <div
-      className={`${classes['button']} ${classes["button-left"]}`}
+      className={`${classes["button"]} ${classes["button-left"]}`}
       onClick={onLeftScrollButtonClick}
     >
       <img
-        className={classes['button-img']}
+        className={classes["button-img"]}
         src="https://assets.website-files.com/60ee32414a2efcb87f4a2940/60ee32414a2efcf93a4a29b3_arrow-scroll.svg"
         alt="LeftButton"
       />
@@ -17,9 +17,9 @@ const LeftScrollButton = ({ onLeftScrollButtonClick }) => {
 
 const RightScrollButton = ({ onRightScrollButtonClick }) => {
   return (
-    <div className={classes['button']} onClick={onRightScrollButtonClick}>
+    <div className={classes["button"]} onClick={onRightScrollButtonClick}>
       <img
-        className={classes['button-img']}
+        className={classes["button-img"]}
         src="https://assets.website-files.com/60ee32414a2efcb87f4a2940/60ee32414a2efcf93a4a29b3_arrow-scroll.svg"
         alt="RightButton"
       />
