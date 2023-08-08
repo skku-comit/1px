210624c934d8130e8e49605534588cc3d98b8c84
aptheparker <aptheparker@gmail.com>
2023-08-08 13:37:50 +0800

    about page codes refactored

--- src/components/layout/TopHeaderBar.js
+++ src/components/layout/TopHeaderBar.js
@@ -1,26 +1,23 @@
-import { Link } from 'react-router-dom';
-import classes from './TopHeaderBar.module.css';
+// imports
+import { Link } from "react-router-dom";
 
-function TopHeaderBar(props) {
+// css
+import classes from "./TopHeaderBar.module.css";
+
+const TopHeaderBar = () => {
   return (
-    <div className={classes.header}>
-      <Link to={'/'} className={classes.linkdecoration}>
-        <div className={classes['logo-container']}>
-          <h1 className={classes.logo}>1PX</h1>
-        </div>
+    <div className={classes["header"]}>
+      <Link to={"/"} className={classes["link-decoration"]}>
+        <div className={classes["logo-container"]}>1PX</div>
       </Link>
-      <Link to={'./about'} className={classes.linkdecoration}>
-        <div className={classes['about-container']}>
-          <p className={classes.about}>About</p>
-        </div>
+      <Link to={"./about"} className={classes["link-decoration"]}>
+        <div className={classes["about-container"]}>About</div>
       </Link>
-      <Link to={'./guest-book'} className={classes.linkdecoration}>
-        <div className={classes['guestbook-container']}>
-          <p className={classes.guestbook}>Guest Book</p>
-        </div>
+      <Link to={"./guest-book"} className={classes["link-decoration"]}>
+        <div className={classes["guestbook-container"]}>Guest Book</div>
       </Link>
     </div>
   );
-}
+};
 
 export default TopHeaderBar;
