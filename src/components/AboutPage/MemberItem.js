61734e8c52a80005e5cc0cbbdd1c2d76f2959f8c
aptheparker <aptheparker@gmail.com>
2023-08-26 19:08:21 +0900

    about member changed image

--- src/components/AboutPage/MemberItem.js
+++ src/components/AboutPage/MemberItem.js
@@ -4,7 +4,9 @@ import classes from "./MemberItem.module.css";
 const MemeberItem = (props) => {
   return (
     <div className={classes.Member_item}>
-      <img className={classes.snowman} src={props.image} alt="member" />
+      <div className={classes.snowman}>
+        <img src={props.image} alt="member" />
+      </div>
       <div className={classes.nameplus}>{props.name}</div>
       <p className={classes.job}>{props.job}</p>
     </div>
